5cfbf8ce1d89d13efb8cd880995daf54cb6279f8
David Santos <dsdsantos973@gmail.com>
2025-01-07 12:09:24 -0500

    add a HBox for the buttons to align horiztonally

--- src/main/java/com/example/crud/HelloController.java
+++ src/main/java/com/example/crud/HelloController.java
@@ -4,10 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.time.LocalDate;
@@ -16,6 +13,7 @@ public class HelloController {
 
     @FXML
     private Label customText;
+    TextField
     @FXML
     private TableView<Tasks> tableView;
     @FXML
